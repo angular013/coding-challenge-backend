eb3339eb61db0996a146467e91866e3fcfabf4b7
Shayan Arif <angular013@gmail.com>
2023-11-13 20:58:11 +0100

    authentication + postman updated

--- microservices/gateway/src/gateway.ts
+++ microservices/gateway/src/gateway.ts
@@ -1,5 +1,5 @@
-import './open-telemetry'; // Import the OpenTelemetry configuration
-import { ApolloGateway, IntrospectAndCompose } from '@apollo/gateway';
+//import './open-telemetry'; // Import the OpenTelemetry configuration
+import { ApolloGateway, IntrospectAndCompose, RemoteGraphQLDataSource } from '@apollo/gateway';
 import { ApolloServerPluginDrainHttpServer , ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 import { ApolloServer } from '@apollo/server';
 import express from "express";
@@ -8,7 +8,15 @@ import { readFileSync } from 'fs';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { BaseContext } from '@apollo/server';
 
-
+class AuthenticatedDataSource extends RemoteGraphQLDataSource {
+  async willSendRequest({ request, context }: { request: any; context: any }) {
+    // Forward the Authorization header from the incoming request to the services
+    const authorizationHeader = context.token;
+    if (authorizationHeader) {
+      request.http.headers.set('authorization', authorizationHeader);
+    }
+  }
+}
 
 const supergraphSdl = readFileSync('./supergraph.graphql').toString();
 
@@ -18,47 +26,50 @@ const prodOrigins = [/^https:\/\/.*\.yourdomain\.com$/];
 
 const gateway = new ApolloGateway({
     supergraphSdl,
+    buildService({ url }) {
+      return new AuthenticatedDataSource({url}); // Custom data source to forward headers
+    },
 });
 
-const myPlugin = {
-    // Fires whenever a GraphQL request is received from a client.
-    async requestDidStart(requestContext: any) {
-      console.log('Request started! Query:\n' + requestContext.request.query);
+// const myPlugin = {
+//     // Fires whenever a GraphQL request is received from a client.
+//     async requestDidStart(requestContext: any) {
+//       console.log('Request started! Query:\n' + requestContext.request.query);
   
-      return {
-        // Fires whenever Apollo Server will parse a GraphQL
-        // request to create its associated document AST.
-        async parsingDidStart(requestContext: any) {
-          console.log('Parsing started!');
-        },
+//       return {
+//         // Fires whenever Apollo Server will parse a GraphQL
+//         // request to create its associated document AST.
+//         async parsingDidStart(requestContext: any) {
+//           console.log('Parsing started!');
+//         },
   
-        // Fires whenever Apollo Server will validate a
-        // request's document AST against your GraphQL schema.
-        async validationDidStart(requestContext: any) {
-          console.log('Validation started!');
-          console.log(requestContext.request)
-        },
-      };
-    },
-  };
+//         // Fires whenever Apollo Server will validate a
+//         // request's document AST against your GraphQL schema.
+//         async validationDidStart(requestContext: any) {
+//           console.log('Validation started!');
+//           console.log(requestContext.request)
+//         },
+//       };
+//     },
+//   };
 
-async function startApolloServer() {
-	const app = express();
-	const httpServer = http.createServer(app);
-	const server = new ApolloServer({
-      gateway,
-      plugins: [myPlugin],
+// async function startApolloServer() {
+// 	const app = express();
+// 	const httpServer = http.createServer(app);
+// 	const server = new ApolloServer({
+//       gateway,
+//       plugins: [myPlugin],
         
-	});
+// 	});
 
-}
+// }
 
 async function startApolloServer02() {
     const server = new ApolloServer({ 
       gateway, 
       });
     const { url } = await startStandaloneServer(server, {
-      context: async ({ req }) => ({ token: req.headers.token }),
+      context: async ({ req }) => ({ token: req.headers.authorization }),
       listen: { port: 4000 },
     });
   
--- microservices/questionnaire/src/context.ts
+++ microservices/questionnaire/src/context.ts
@@ -2,6 +2,7 @@ import { PrismaClient } from '../prisma/generated/clientQuestionnaire';
 import { ExpressContext } from "apollo-server-express/dist/ApolloServer";
 import { Request, Response } from "express";
 import { prisma } from "./clients";
+import { GraphQLError } from 'graphql';
 
 export interface Context {
 	request: Request;
@@ -9,9 +10,21 @@ export interface Context {
 	prisma: PrismaClient;
 }
 
-export async function createContext(
-	request: ExpressContext
-): Promise<Partial<Context>> {
+export async function createContext(request: ExpressContext): Promise<Partial<Context>> {
+
+	const token = request.req.headers.authorization || '';
+
+	if (token !== "Bearer adminTokenPassword") {
+		console.error('User is not authenticated');
+		throw new GraphQLError('User is not authenticated', {
+			extensions: {
+			  code: 'UNAUTHENTICATED',
+			  http: { status: 401 },
+			},
+		});
+	}
+
+
 	const context: Context = {
 		...request,
 		response: request.res,
